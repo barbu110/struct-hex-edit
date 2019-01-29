17959f866b9c23549e59e7d3be055da845eaf06c
Victor Barbu <victorbarbu08@gmail.com>
2019-01-29 19:57:49 +0200

    Initial Commit

---
+++ StructHexEdit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StructHexEdit
+//
+//  Created by Victor Barbu on 29/01/2019.
+//  Copyright © 2019 Victor Barbu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StructHexEdit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StructHexEdit
+//
+//  Created by Victor Barbu on 29/01/2019.
+//  Copyright © 2019 Victor Barbu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
